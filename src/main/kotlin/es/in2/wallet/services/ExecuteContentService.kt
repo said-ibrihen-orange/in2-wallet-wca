8cd91c41af0e41cc2ebe3e29d8848cb24a1cb809
IN2\haopengly <haopeng.lin@in2.es>
2023-05-03 13:59:24 +0200

    Hotfix Demo-2

--- src/main/kotlin/es/in2/wallet/services/ExecuteContentService.kt
+++ src/main/kotlin/es/in2/wallet/services/ExecuteContentService.kt
@@ -30,21 +30,7 @@ class ExecuteContentImpl : ExecuteContentService {
     }
 
     override fun sendAuthenticationResponse(siopAuthenticationRequest: String, vp: String): String {
-        /*
-        val formData = "state=$state" +
-                "&vp_token=$vp" +
-                "&presentation_submission=" +
-                "%7B%22definition_id%22%3A%22" +
-                "CustomerPresentationDefinition%22%2C%22" +
-                "descriptor_map%22%3A%5B%7B%22" +
-                "format%22%3A%22" +
-                "jwt_vp%22%2C%22id%22%3A%22" +
-                "id_credential%22%2C%22" +
-                "path%22%3A%22%24%22%2C%22" +
-                "path_nested%22%3A%7B%22" +
-                "format%22%3A%22" +
-                "jwt_vc%22%2C%22path%22%3A%22%24.verifiableCredential%5B0%5D%22%7D%7D%5D%2C%22id%22%3A%22" +
-                "CustomerPresentationSubmission%22%7D&id_token=ADASD"*/
+
         val jwt = SignedJWT.parse(siopAuthenticationRequest)
         println("JWT: ${jwt.jwtClaimsSet}")
         val authRequest = jwt.payload.toJSONObject()["auth_request"]
@@ -58,21 +44,37 @@ class ExecuteContentImpl : ExecuteContentService {
         val redirectUri = redirectUriMatchResult?.value ?: throw IllegalArgumentException("Missing 'redirect_uri' parameter")
 
 
-
+/*
         val formData = HashMap<String, String>()
         formData["state"] = state
         formData["vp_token"] = vp
+        formData["presentation_submission"] = "{\"definition_id\":\"CustomerPresentationDefinition\",\"descriptor_map\":[{\"format\":\"jwt_vp\",\"id\":\"id_credential\",\"path_nested\":{\"path\":\"$.verifiableCredential[0]\"},\"}],\"id\":\"CustomerPresentationSubmission\"}"
         formData["presentation_submission"] = "{\"definition_id\":\"CustomerPresentationDefinition\",\"descriptor_map\":[{\"format\":\"jwt_vp\",\"id\":\"id_credential\",\"path\":\"$\"}],\"id\":\"CustomerPresentationSubmission\"}"
-        formData["id_token"] = "ADASD"
+        formData["id_token"] = "ADASD"*/
 
-        // TODO Hao, This will be retrieved from redirect_uri attribute of SIOP Authentication Request
-        //val endpointUrl = "${walletProperties.domeBackendBaseURL}/relying-party/siop-sessions"
+
+        val formData = "state=$state" +
+                "&vp_token=$vp" +
+                "&presentation_submission=" +
+                "%7B%22definition_id%22%3A%22" +
+                "CustomerPresentationDefinition%22%2C%22" +
+                "descriptor_map%22%3A%5B%7B%22" +
+                "format%22%3A%22" +
+                "jwt_vp%22%2C%22id%22%3A%22" +
+                "id_credential%22%2C%22" +
+                "path%22%3A%22%24%22%2C%22" +
+                "path_nested%22%3A%7B%22" +
+                "format%22%3A%22" +
+                "jwt_vc%22%2C%22path%22%3A%22%24.verifiableCredential%5B0%5D%22%7D%7D%5D%2C%22id%22%3A%22" +
+                "CustomerPresentationSubmission%22%7D&id_token=ADASD"
+        println("Redirect URI: $redirectUri")
 
         val client = HttpClient.newBuilder().build()
         val request = HttpRequest.newBuilder()
             .headers("Content-Type", "application/x-www-form-urlencoded")
-            .uri(URI.create(redirectUri))
-            .POST(HttpRequest.BodyPublishers.ofString(formData.map { "${it.key}=${it.value}" }.joinToString("&")))
+            .uri(URI.create(/*redirectUri*/"http://localhost:8082/api/verifier/siop-sessions"))
+            //.POST(HttpRequest.BodyPublishers.ofString(formData.map { "${it.key}=${it.value}" }.joinToString("&")))
+            .POST(HttpRequest.BodyPublishers.ofString(formData))
             .build()
 
         val response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
