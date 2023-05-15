6142cff5918b8ba8dba33da31bf6113c7930f6c7
oriol.canades@in2.es <oriol.canades@in2.es>
2023-05-15 12:10:27 +0200

    FIX SonarCloud comment NEW Application Test

--- src/test/kotlin/es/in2/wallet/WalletBackendApplicationTests.kt
+++ src/test/kotlin/es/in2/wallet/WalletBackendApplicationTests.kt
@@ -1,8 +1,14 @@
 package es.in2.wallet
 
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class WalletBackendApplicationTests {
+@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WalletBackendApplicationTests() {
+
+    @Test
+    fun contextLoads() {
+        // This test ensures that the application context loads without errors
+    }
 
 }
