879e3f42d7e52bb8680958229d02a22614f755a6
IN2\haopengly <haopeng.lin@in2.es>
2023-06-13 09:48:04 +0200

    rename

--- src/main/kotlin/es/in2/wallet/model/dto/contextBroker/ContextBrokerAttributeDTO.kt
+++ src/main/kotlin/es/in2/wallet/model/dto/contextBroker/ContextBrokerAttributeDTO.kt
@@ -1,6 +1,6 @@
 package es.in2.wallet.model.dto.contextBroker
 import com.fasterxml.jackson.annotation.JsonProperty
-class ContextBrokerAtributeDTO (
+class ContextBrokerAttributeDTO (
     @JsonProperty("type") val type: String,
     @JsonProperty("value") val value: Any,
     @JsonProperty("metadata") val metadata: Any?
